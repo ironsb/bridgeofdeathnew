db37313a8d27f1a65577f5dbce9250323465cb07
ironsb <ironsb@pagosa.k12.co.us>
2014-09-30 11:06:58 -0600

    Advanced Program

---
+++ bridgeofdeathnew/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnApproach = New System.Windows.Forms.Button()
+        Me.lblFate = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnApproach
+        '
+        Me.btnApproach.Location = New System.Drawing.Point(77, 70)
+        Me.btnApproach.Name = "btnApproach"
+        Me.btnApproach.Size = New System.Drawing.Size(133, 23)
+        Me.btnApproach.TabIndex = 0
+        Me.btnApproach.Text = "Approach the Bridge"
+        Me.btnApproach.UseVisualStyleBackColor = True
+        '
+        'lblFate
+        '
+        Me.lblFate.AutoSize = True
+        Me.lblFate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFate.Location = New System.Drawing.Point(104, 188)
+        Me.lblFate.Name = "lblFate"
+        Me.lblFate.Size = New System.Drawing.Size(0, 24)
+        Me.lblFate.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblFate)
+        Me.Controls.Add(Me.btnApproach)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnApproach As System.Windows.Forms.Button
+    Friend WithEvents lblFate As System.Windows.Forms.Label
+
+End Class
